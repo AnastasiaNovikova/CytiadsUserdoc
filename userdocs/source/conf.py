5b3d8e3a19624e97cb590e8484ec71e106f255f5
AnastasiaNovikova <anastasia.novikova@mail.com>
2016-07-25 13:10:08 +0300

    Update conf.py

--- userdocs/source/conf.py
+++ userdocs/source/conf.py
@@ -135,7 +135,7 @@ html_theme_path = ["_themes"]
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = ['favicon.ico']
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -192,7 +192,7 @@ html_static_path = ['_themes\sphinx_rtd_theme\static']
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
+html_search_language = 'ru'
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
